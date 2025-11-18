b76bfb0dacb4a1f6e359ac964c8c6a4c64a2f130
torham <torhamdev@gmail.com>
2025-11-18 19:48:29 +0330

    Fix date format in README and date display function

--- display/date.py
+++ display/date.py
@@ -5,5 +5,5 @@ from configs.constants import DATE_TITLE
 
 def show_current_date():
     """Display the current date in a formatted manner."""
-    result = DATE_TITLE % datetime.now().strftime("%Y-%M-%d")
+    result = DATE_TITLE % datetime.now().strftime("%Y-%m-%d")
     print(result.center(80))
